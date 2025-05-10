cb921118471408817a788864de83d64cb8d77f0c
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-05-10 15:19:14 -0700

    move exercise pt 2

--- move.js
+++ move.js
@@ -1,9 +1,23 @@
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 5, 6];
 
-const output = move(numbers, 0, 0);
+const output = move(numbers, 0, 2);
 
 console.log(output);
 
 function move(array, index, offset){
+    const position = index + offset;
+
+    if (position < 0 || position >= array.length){
+        console.error('Invalid offset.')
+        return;
+    }
+
+    const output = [...array];
+    const element = output.splice(index, 1)[0];
+    output.splice(position, 0, element);
+
+    return output
+
+
     
 }
