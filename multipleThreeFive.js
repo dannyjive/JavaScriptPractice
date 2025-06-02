4702ff805aa5c045a1a7e784e9a20b58d3ef4fb9
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-06-01 20:10:26 -0700

    added multipleThreeFive

---
+++ multipleThreeFive.js
@@ -0,0 +1,12 @@
+function solution(number) {
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    if (number < 0) {
+      i = 0;
+    }
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
