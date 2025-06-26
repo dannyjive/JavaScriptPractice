c9e6aed82ffbc3309fd193d2093d0f7fc4ac1578
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-06-25 20:41:00 -0700

    sumOfStrings

---
+++ sumOfStrings.js
@@ -0,0 +1,8 @@
+function sumStr(a,b) {
+    return ((+a ||0 ) + (+b || 0)).toString();
+//  let intOne = parseInt(a) || 0;
+//  let intTwo = parseInt(b) || 0;
+//  let sum = intOne + intTwo;
+ 
+//  return sum.toString();
+}
