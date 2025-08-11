2842bba37f9c8a7bd4954b918539245d543a3dda
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-08-10 19:21:20 -0700

    open or senior

---
+++ openOrSenior.js
@@ -0,0 +1,16 @@
+function openOrSenior(data){
+
+  const status = []
+  
+  for (let elem of data){
+    if (elem[0] >= 55 && elem[1] > 7){
+      status.push("Senior")
+    } else {
+      status.push("Open")
+    }
+  }
+  
+  return status;
+}
+
+openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])
