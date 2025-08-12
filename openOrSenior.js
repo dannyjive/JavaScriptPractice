305dfe9e13296cef49469cfc391c8b99b7d2014c
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-08-11 20:58:02 -0700

    simple mult

--- openOrSenior.js
+++ openOrSenior.js
@@ -1,16 +1,25 @@
-function openOrSenior(data){
+function openOrSenior(data) {
+  const status = [];
 
-  const status = []
-  
-  for (let elem of data){
-    if (elem[0] >= 55 && elem[1] > 7){
-      status.push("Senior")
+  for (let elem of data) {
+    if (elem[0] >= 55 && elem[1] > 7) {
+      status.push("Senior");
     } else {
-      status.push("Open")
+      status.push("Open");
     }
   }
-  
+
   return status;
 }
 
-openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])
+// Simpler Way
+// function openOrSenior(data){
+//   return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+// }
+
+openOrSenior([
+  [45, 12],
+  [55, 21],
+  [19, -2],
+  [104, 20],
+]);
---
+++ simpleMult.js
@@ -0,0 +1,3 @@
+function simpleMultiplication(number) {
+    return number % 2 === 0 ? 8 * number : 9 * number;
+}
