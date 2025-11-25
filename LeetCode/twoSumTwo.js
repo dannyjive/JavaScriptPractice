d978294d2eae20ae99ebde79e30dd58473e3db63
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-11-25 12:07:48 -0800

    added leetcode

---
+++ LeetCode/twoSumTwo.js
@@ -0,0 +1,19 @@
+// Keep space O(1)
+
+var twoSum = function(numbers, target){
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+
+        if (sum === target) {
+            return [left +1, right + 1];
+        }
+        if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+};
---
+++ LeetCode/validPalindrome.js
@@ -0,0 +1,26 @@
+// Time complexity: O(n)
+// Space complexity: O(1) (ignoring regex engine)
+
+var isPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !isAlphaNumeric(s[left])) left++;
+        while (left < right && !isAlphaNumeric(s[right])) right--;
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+        left++;
+        right--;
+
+    }
+
+    return true;
+
+};
+
+function isAlphaNumeric(c) {
+    return /[a-z0-9]/i.test(c);
+}
