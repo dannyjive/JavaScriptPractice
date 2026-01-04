f0c1d57415567d32b99bd54ecce486fa855e7412
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2026-01-03 19:12:48 -0800

    added alternatingCase.js

---
+++ alternatingCase.js
@@ -0,0 +1,10 @@
+String.prototype.toAlternatingCase = function () {
+  return this
+    .split("")
+    .map(char =>
+      char === char.toUpperCase()
+        ? char.toLowerCase()
+        : char.toUpperCase()
+    )
+    .join("");
+};
